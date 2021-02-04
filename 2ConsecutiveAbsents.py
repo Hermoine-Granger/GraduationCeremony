38441a3080c2371209e368df22664d57c382502f
Himani Sharma <hsharma316@gmail.com>
2021-02-04 09:18:03 +0530

    Update 2ConsecutiveAbsents.py

--- 2ConsecutiveAbsents.py
+++ 2ConsecutiveAbsents.py
@@ -2,16 +2,16 @@
 
 n=int(input())
 	
-ways=[0,0,1,1]
-hard_coded_last_idx=3
+ways=[0,0,1]
+hard_coded_size=3
 
-for i in range(1,n+1):
-    i=i+hard_coded_last_idx
+for i in range(0,n+1):
+    i=i+hard_coded_size
     ways.append(ways[i-1]+ways[i-2]+ways[i-3])
 	
-n+=hard_coded_last_idx    
+n+=hard_coded_size    
 total_ways=ways[n]
-if n<=2*hard_coded_last_idx:
+if n<=2*hard_coded_size:
 	not_attend=ways[n-1]
 else:
 	not_attend=ways[n-2]+ways[n-3]
