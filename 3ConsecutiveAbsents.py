c56aaebed28863fc4c5d8adcb450226f405a3cd4
Himani Sharma <hsharma316@gmail.com>
2021-02-04 09:09:25 +0530

    Update 3ConsecutiveAbsents.py

--- 3ConsecutiveAbsents.py
+++ 3ConsecutiveAbsents.py
@@ -1,21 +1,22 @@
 # 3 consecutive absents allowed
 
 n=int(input())
-if n==1:
-    print("Possible ways= ",1)
-    print("Probability = ",str(0)+'\\'+str(1))
 	
 ways=[0,0,0,1,1]
+hard_coded_last_idx=4
 
 for i in range(1,n+1):
     i=i+4
     ways.append(ways[i-1]+ways[i-2]+ways[i-3]+ways[i-4])
 
-n+=4	
+n+=hard_coded_last_idx	
 total_ways=ways[n]
-not_attend=ways[n-2]+ways[n-3]+ways[n-4]
+if n<=2*hard_coded_last_idx:
+	not_attend=ways[n-1]
+else:
+	not_attend=ways[n-2]+ways[n-3]+ways[n-4]
 
 print("(",total_ways,end=',')
 print(str(not_attend)+'\\'+str(total_ways)+")")
-print()
+
 
